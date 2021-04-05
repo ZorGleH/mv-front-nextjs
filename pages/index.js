fbb388618a12b599bf44c3244785376e6c2a4824
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2021-04-05 17:56:34 +0200

    test from the server side

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,36 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export async function getServerSideProps() {
+    // Fetch data from external API
+    const res = await fetch('https://randomuser.me/api/')
+    const users = await res.json()
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+    // Pass data to the page via props
+    return { props: { users } }
+  }
 
-      <Footer />
-    </div>
-  )
+const Home = ({ users }) => {
+    // this console log appears on the server side, not on the client side!
+    console.log(users)
+    return (
+      <div className="container">
+        <Head>
+          <title>This is fetch on the server</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main>
+          <Header title="Fetched from the server" />
+          { users && users.results.map(( user, index) => 
+          <p key={index} className="description">
+            {`${user.name.first} ${user.name.last}` } 
+          </p>
+          ) }
+        </main>
+
+        <Footer />
+      </div>
+    )
 }
+export default Home
